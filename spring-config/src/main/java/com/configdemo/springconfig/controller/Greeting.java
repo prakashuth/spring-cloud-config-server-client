3fe4fc663cd545588764e8d2ebb514c36bfed17b
prakashuth <59371851+prakashuth@users.noreply.github.com>
2020-05-08 14:06:29 -0500

    initial commit

---
+++ spring-config/src/main/java/com/configdemo/springconfig/controller/Greeting.java
@@ -0,0 +1,47 @@
+package com.configdemo.springconfig.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.configdemo.springconfig.model.UserDetails;
+
+@RestController
+@RefreshScope
+public class Greeting {
+	
+	@Value("${greeting:default hwllow world}")
+	private String greeting;
+	@Value("${list:default,hwllow,world}")
+
+	private List<String> listValues;
+	
+	@Value("#{${mapValues}}")
+
+	private Map<String,String> mapValues;
+	@Autowired
+	private UserDetails userDetails;
+
+	@RequestMapping("/greetings")
+	public String getGreetings(){
+		 
+		return greeting+listValues.size()+mapValues + userDetails.getPassword();
+		
+	}
+	@Autowired
+	Environment environment;
+
+	 @GetMapping("/env")
+		public String getgreeting(){
+			 
+			return environment.toString();
+			
+		}
+}
---
+++ spring-config/src/main/java/com/configdemo/springconfig/model/UserDetails.java
@@ -0,0 +1,44 @@
+package com.configdemo.springconfig.model;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("user")
+public class UserDetails {
+	
+	private List<String> details;
+	private int pwdage;
+	private String password;
+	/*@Value("#{${user.mapValues}}")
+	private Map<String,String> mapValues;*/
+
+	
+	public int getPwdage() {
+		return pwdage;
+	}
+	public void setPwdage(int pwdage) {
+		this.pwdage = pwdage;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<String> getDetails() {
+		return details;
+	}
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+	/*public Map<String, String> getMapValues() {
+		return mapValues;
+	}
+	public void setMapValues(Map<String, String> mapValues) {
+		this.mapValues = mapValues;
+	}*/
+
+}
